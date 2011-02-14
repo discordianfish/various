fbca54f3cb29022e7360f1b41d13f22d28e8df5d
Johannes 'fish' Ziemke <fish@freigeist.org>
2011-02-14 17:07:22 +0100

    uses readdir instead of simple globbing for supporting files with spaces

--- fbupload.pl
+++ fbupload.pl
@@ -6,6 +6,7 @@ use constant API_KEY => '5346da9a42bc26daa40d3aad9866e71b';
 use constant API_SECRET => 'f71d99d43be0ffec625028e1bd1a35c2';
 
 use File::Basename;
+use File::Spec;
 use WWW::Facebook::API;
 
 my @PATHS = @ARGV;
@@ -34,27 +35,43 @@ for my $path (@PATHS)
     print "uploading files in $path\n";
     my $AID = $ENV{AID} ? $ENV{AID} : ($client->photos->create_album(name => basename $path))->{aid}
         or die 'could not create new album';
-    
-    for my $file (<$path/*>)
+ 
+    opendir(my $dir, $path);
+    unless ($dir)
     {
-        open my $fh, '<', $file;
+        warn "could not readdir $dir";
+        next;
+    }
+        
+    while (my $file = readdir $dir)
+    {
+        next if $file eq '.';
+        next if $file eq '..';
+
+        open my $fh, '<', File::Spec->catdir($path, $file);
         unless ($fh)
         {
             warn "could not read $file";
             next;
         }
-        my ($name, $path, $suffix) = fileparse $file;
     
         my $data;
         while ($fh->sysread(my $buffer, 256000)) { $data .= $buffer };
         close $fh;
+
+        unless ($data)
+        {
+            warn "no data in $file, skipping";
+            next;
+        }
         
         print "   uploading $file\n";
         $client->photos->upload
         (
             aid => $AID,
-            caption => $name,
+            caption => $file,
             data => $data,
         );
     }
+    close $dir;
 }
